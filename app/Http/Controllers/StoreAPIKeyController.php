17dcaaa52620977978aaa818aeb0b7fb344b1b52
Marc Anthony <147101133+marcanthonysurmont@users.noreply.github.com>
2024-12-19 17:37:21 +0100

    Create StoreAPIKeyController.php

---
+++ app/Http/Controllers/StoreAPIKeyController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Information;
+
+class StoreAPIKeyController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $validated = $request->validate([
+            'api_key' => 'required|string'
+        ]);
+
+        $apiKey = $validated['api_key'];
+
+        $information = Information::first();
+
+        if ($information) {
+            $information->update(['api_key' => $apiKey]);
+        }
+
+        if(!$information) {
+            Information::create(['api_key' => $apiKey]);
+        }
+
+        return response()->json(['success' => 'API Key stored successfully']);
+    }
+}
