dc170c33878f96add91dc617aba30e8e59ade70b
Marc Anthony <marc.surmont@student.kdg.be>
2024-12-19 22:16:25 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ViewHomeController;
 use App\Http\Controllers\StoreAPIKeyController;
+use App\Http\Controllers\StorePromptController;
 
 Route::get('/', ViewHomeController::class)->name('home');
-Route::post('/information', StoreAPIKeyController::class)->name('store.apikey');
+Route::post('/information/apikey', StoreAPIKeyController::class)->name('store.apikey');
+Route::post('/information/prompt', StorePromptController::class)->name('store.prompt');
