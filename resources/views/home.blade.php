6d8f98ac7c9b681b3618d739cb91c89cabd4ee23
Marc Anthony <147101133+marcanthonysurmont@users.noreply.github.com>
2024-12-19 17:48:42 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,9 +7,26 @@
         <title>Rewrite GPT</title>
 
         <!-- Styles / Scripts -->
-    
+
     </head>
     <body>
-        
+        <h1>Rewrite GPT</h1>
+
+        <h2>API Key : </h2>
+        <form action="{{ route('store.apikey') }}" method="POST">
+            @csrf
+            <input type="text" name="api_key" placeholder="Enter your OpenAI API key here..." value="{{ $information->api_key }}">
+            <button type="submit">Submit</button>
+        </form>
+
+        <h2>Options : </h2>
+        <form action="" method="POST">
+            @csrf
+            <input type="radio" name="options" value="1"> Rewrite
+            <br>
+            <input type="radio" name="options" value="2"> Translate
+            <br>
+            <button type="submit">Save</button>
+        </form>
     </body>
 </html>
