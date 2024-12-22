748e15e7fc7320b1c6d0fc593494be79bc72f415
Marc Anthony <marc.surmont@student.kdg.be>
2024-12-22 14:30:13 +0100

    Create JsonResponseListener.php

---
+++ app/Listeners/JsonResponseListener.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\JsonResponseEvent;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Log;
+ 
+
+class JsonResponseListener
+{
+    /**
+     * Handle the event.
+     */
+    public function handle(JsonResponseEvent $event): void
+    {
+        $response = $event->response;
+        
+        $response = json_encode($response);
+
+        Log::info($response);
+    }
+}
