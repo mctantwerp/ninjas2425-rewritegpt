4c2b743465ae74d482af537e29b15f19f47bee3a
Marc Anthony <marc.surmont@student.kdg.be>
2024-12-22 21:50:04 +0100

    Update

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Rewrite GPT'),
+    'name' => env('APP_NAME', 'RewriteGPT'),
 
     /*
     |--------------------------------------------------------------------------
